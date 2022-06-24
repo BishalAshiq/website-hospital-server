5e2412719ea80f7288c7e88012cea00b2a03b0dc
BishalAshiq <bishaltheboss20@gmail.com>
2022-06-24 23:49:43 +0600

    added server

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient} = require('mongodb')
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.98ro8.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true});
+
+
+app.get('/', (req, res) => {
+  res.send('Hello! From Raditon Hospital!')
+})
+
+app.listen(port, () => {
+  console.log(`listening on ${port}`)
+}) 
